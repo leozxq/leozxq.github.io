2b0f70d52f7a400b9224e2d7b373e8232878c6ba
leozxq <461764293@qq.com>
2023-12-22 00:18:15 +0800

    reset coding

--- src/pages/Tank.vue
+++ src/pages/Tank.vue
@@ -9,92 +9,106 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 // import gsap from "gsap";
 import GUI from "lil-gui";
 
-const loadingManager = new THREE.LoadingManager();
-const textureloader = new THREE.TextureLoader(loadingManager);
-const cubeTextureLoader = new THREE.CubeTextureLoader();
-const envTexture = cubeTextureLoader
-  .setPath("/img/")
-  .load([
-    "OIP-C.jpg",
-    "OIP-C.jpg",
-    "OIP-C.jpg",
-    "OIP-C.jpg",
-    "OIP-C.jpg",
-    "OIP-C.jpg",
-  ]);
-const texture = textureloader.load("/img/1.png");
+let lightHelper;
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
-// texture.center.x = Math.PI / 4;
-// texture.repeat.x = 2;
-// texture.repeat.y = 3;
-// texture.wrapS = THREE.MirroredRepeatWrapping;
-// texture.wrapT = THREE.MirroredRepeatWrapping;
 const scene = new THREE.Scene();
+// glass geometry build
 const tankGroup = new THREE.Group();
-const geometry2 = new THREE.SphereGeometry(0.1, 16, 16);
-const material2 = new THREE.MeshPhongMaterial();
-const geoFront = new THREE.BoxGeometry(6, 4, 0.06);
-
-const geoback = new THREE.BoxGeometry(6, 4, 0.06);
-const geoLeft = new THREE.BoxGeometry(0.06, 4, 4);
-
-const geoRight = new THREE.BoxGeometry(0.06, 4, 4);
-
-const geoEnd = new THREE.BoxGeometry(6, 0.06, 4);
-
-const material = new THREE.MeshBasicMaterial({
+const frontBack = new THREE.BoxGeometry(6, 4, 0.06);
+const leftRight = new THREE.BoxGeometry(0.06, 4, 4);
+const end = new THREE.BoxGeometry(6, 0.06, 4);
+const glassMaterial = new THREE.MeshStandardMaterial({
   color: 0xadd8e6, // 浅蓝色，像玻璃的颜色
   transparent: true,
   opacity: 0.2, // 半透明
 });
-// material.map = texture;
-// material.envMap = envTexture;
-// material.envMapIntensity = 1.0;
-const mesh1 = new THREE.Mesh(geoFront, material);
+const mesh1 = new THREE.Mesh(frontBack, glassMaterial);
 mesh1.position.z = 2;
-const mesh2 = new THREE.Mesh(geoback, material);
+const mesh2 = new THREE.Mesh(frontBack, glassMaterial);
 mesh2.position.z = -2;
-const mesh3 = new THREE.Mesh(geoLeft, material);
+const mesh3 = new THREE.Mesh(leftRight, glassMaterial);
 mesh3.position.x = -3;
-const mesh4 = new THREE.Mesh(geoRight, material);
+const mesh4 = new THREE.Mesh(leftRight, glassMaterial);
 mesh4.position.x = 3;
-const mesh5 = new THREE.Mesh(geoEnd, material);
+const mesh5 = new THREE.Mesh(end, glassMaterial);
 mesh5.position.y = -2;
-const meshBall = new THREE.Mesh(geometry2, material2);
 tankGroup.add(mesh1, mesh2, mesh3, mesh4, mesh5);
-scene.add(tankGroup, meshBall);
-const pointLight = new THREE.PointLight(0xffffff, 1000, 2000.0);
-pointLight.position.set(0, 10, 0);
-scene.add(pointLight);
+tankGroup.castShadow = true;
+tankGroup.receiveShadow = true;
+// plane geometry build
+const planeGeometry = new THREE.PlaneGeometry(8, 8);
+const planeMaterial = new THREE.MeshStandardMaterial({
+  side: THREE.DoubleSide,
+  color: 0x3cfb3c,
+});
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+planeMesh.position.y = -2;
+planeMesh.rotateX(Math.PI / 2);
+planeMesh.receiveShadow = true;
+scene.add(tankGroup, planeMesh);
+// small ball build
+const smallBallGeometry = new THREE.SphereGeometry(0.1, 16, 16);
+const smallBallMaterial = new THREE.MeshStandardMaterial({ color: 0x3d81f0 });
+for (let i = 0; i < 300; i++) {
+  const meshBall = new THREE.Mesh(smallBallGeometry, smallBallMaterial);
+  meshBall.castShadow = true;
+  meshBall.position.x = (Math.random() - 0.5) * 15;
+  meshBall.position.y = (Math.random() - 0.5) * 15;
+  meshBall.position.z = (Math.random() - 0.5) * 15;
+  scene.add(meshBall);
+}
+// light build
+const spotLight = new THREE.SpotLight(0xffffff, 10);
+spotLight.distance = 30;
+spotLight.angle = Math.PI / 4;
+spotLight.decay = 0;
+spotLight.position.set(0, 10, 0);
+spotLight.castShadow = true;
+spotLight.shadow.mapSize.set(1024, 1024);
+spotLight.shadow.camera.near = 0.5; // default
+spotLight.shadow.camera.far = 20; // default
+spotLight.shadow.camera.fov = 50;
+spotLight.shadow.focus = 1;
+scene.add(spotLight);
+// ambient light
 const ambient = new THREE.AmbientLight(0xffffff, 0.4);
 scene.add(ambient);
+// all helper
+const cameraHelper = new THREE.CameraHelper(spotLight.shadow.camera);
 const axesHelper = new THREE.AxesHelper(2);
-scene.add(axesHelper);
-
-for (let i = 0; i < 300; i++) {
-  const meshball = new THREE.Mesh(geometry2, material2);
-  meshball.position.x = (Math.random() - 0.5) * 15;
-  meshball.position.y = (Math.random() - 0.5) * 15;
-  meshball.position.z = (Math.random() - 0.5) * 15;
-  scene.add(meshball);
-}
-
-const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
-};
+scene.add(cameraHelper, axesHelper);
+// camera build
 const camera = new THREE.PerspectiveCamera(
   75,
-  sizes.width / sizes.height,
+  window.innerWidth / window.innerHeight,
   1,
   1000
 );
 camera.position.set(4, 3, 9);
-camera.lookAt(mesh5.position);
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(sizes.width, sizes.height);
+camera.lookAt(tankGroup.position);
 renderer.render(scene, camera);
-renderer.setPixelRatio(window.devicePixelRatio);
+// GUI build
+const gui = new GUI();
+const glassFolder = gui.addFolder("glass");
+const spotLightFolder = gui.addFolder("spotLight");
+const planeFolder = gui.addFolder("plane");
+// glassFolder.close();
+glassFolder.add(tankGroup.position, "x", -3, 3, 0.01);
+glassFolder.add(glassMaterial, "wireframe");
+glassFolder.addColor(glassMaterial, "color");
+glassFolder.add(glassMaterial, "opacity").min(0).max(1).step(0.001);
+glassFolder.add(glassMaterial, "metalness").min(0).max(1).step(0.001);
+glassFolder.add(glassMaterial, "roughness").min(0).max(1).step(0.001);
+planeFolder.addColor(planeMaterial, "color").name("MaterialColor");
+spotLightFolder.add(cameraHelper, "visible");
+spotLightFolder.addColor(spotLight, "color").name("spotLightColor");
+
+// adjust window size automatic
 window.addEventListener("resize", () => {
   sizes.width = window.innerWidth;
   sizes.height = window.innerHeight;
@@ -102,14 +116,7 @@ window.addEventListener("resize", () => {
   camera.updateProjectionMatrix();
   renderer.setSize(sizes.width, sizes.height);
 });
-const gui = new GUI();
-gui.add(tankGroup.position, "x", -3, 3, 0.01);
-// gui.add(material, "metalness").min(0).max(1).step(0.001);
-// gui.add(mesh, "visible");
-gui.add(material, "wireframe");
-gui.addColor(material, "color");
-gui.add(material, "opacity").min(0).max(1).step(0.001);
-// gui.add(material, "roughness").min(0).max(1).step(0.001);
+// appendChild for DOM
 const guiControl = ref();
 onMounted(() => {
   const controls = new OrbitControls(camera, guiControl.value);
